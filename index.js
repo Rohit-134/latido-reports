46f39426ce562b6f8eb6232384a5e4e7e7c8a07f
rohit133 <rohit.12012980@lpu.in>
2023-10-06 17:10:50 +0530

    New Report Archives link

--- index.js
+++ index.js
@@ -23,19 +23,32 @@ function submitForm() {
   var month = datearray[1];
   var day = datearray[2];
 
+
+  // Getting the Date
   var monthname = monthNames[parseInt(month) - 1].substring(0, 3).toUpperCase();
   var reportID = day + "" + monthname + "" + year;
   console.log(reportID)
-  fetchReport(reportID);
+  
+  // Getting the Browser name 
+  var browser = document.getElementById("browserinput");
+  var browser_name = browser.value;
+  console.log(browser_name);
+  
+  fetchReport(reportID, browser_name);
+
 }
+
+// v2 Url with browser name 
+//    .get(`https://rohitfrugal.github.io/Bottle-Allure-Results/${browser_name}/${reportID}/`)
+
 
 const fetchReport = (reportID) => {
   axios
-    .get("https://rohitfrugal.github.io/Bottle-Allure-Results/" + reportID + "/")
+    .get("https://Rohit-134.github.io/latido-report-archives/" + reportID + "/")  // Chnage this url for the post 
     .then((response) => {
       console.log(response);
       window.open(
-        "https://rohitfrugal.github.io/Bottle-Allure-Results/" + reportID + "/"
+        "https://Rohit-134.github.io/latido-report-archives/" + reportID + "/"
       );
     })
     .catch((error) => {
